cd498ccadc8ac253f8daf23c93d7c87545a6b51b
Aline Bessa <alibezz@gmail.com>
2019-08-30 17:00:48 -0400

    implementing a few more unidimensional features. now i have to implement some metalearning statistical features, including pairwise correlations

--- improvement-prediction/feature_factory.py
+++ improvement-prediction/feature_factory.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import stats
 
 # TODO get more unidimensional features, using this metalearning
 # document as reference: https://arxiv.org/pdf/1810.03548.pdf
@@ -9,10 +10,15 @@ class FeatureFactory:
         self.number_of_rows = self.data.shape[0]
         self.row_to_column_ratio = self.data.shape[0]/self.data.shape[1]
         self.number_of_numerical_columns = self.get_number_of_numerical_columns()
-        self.means_of_numerical_columns = self.get_means_of_numerical_columns()
+        self.means_of_numerical_columns = self.get_means_of_numerical_columns() 
+        self.percentages_of_missing_values = self.get_percentages_of_missing_values()
+        self.outlier_percentages_of_numerical_columns = self.get_outlier_percentages_of_numerical_columns()
+
         print('number of columns', self.number_of_columns, 'number of rows', self.number_of_rows,
               'ratio', self.row_to_column_ratio, 'number of numerical columns', self.number_of_numerical_columns,
-              'means of numerical columns', self.means_of_numerical_columns)
+              'means of numerical columns', self.means_of_numerical_columns, 'percentages of missing values',
+              self.percentages_of_missing_values, 'outlier percentages of numerical columns',
+              self.outlier_percentages_of_numerical_columns)
 
     def get_number_of_numerical_columns(self):
         return len([i for i in self.data.dtypes if i == np.int64 or i == np.float64])
@@ -24,3 +30,21 @@ class FeatureFactory:
                 means[column] = self.data[column].mean()
         return means
 
+    def get_percentages_of_missing_values(self):
+        percentage_of_missing_values = {}
+        for column in self.data:
+            percentage_of_missing_values[column] = self.data[column].isnull().sum()/self.data.shape[0]
+        return percentage_of_missing_values
+
+    def get_outlier_percentages_of_numerical_columns(self):
+        outlier_percentages = {}
+        for column in self.data:
+            if self.data[column].dtype == np.int64 or self.data[column].dtype == np.float64:
+                outlier_percentages[column] = len([i for i in stats.zscore(self.data[column]) if np.fabs(i) > 3])/self.data.shape[0]
+        return outlier_percentages
+
+    def get_skewness_of_numerical_columns(self):
+        '''
+        TODO
+        '''
+        pass
