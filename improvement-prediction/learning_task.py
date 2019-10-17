ca841806b9e20525db7a40067797b8b5752a0a7d
Aline Bessa <alibezz@10-18-98-209.dynapool.wireless.nyu.edu>
2019-10-17 16:00:59 -0400

    adding temporary documents with partial results and TODO (will remove later)

--- improvement-prediction/learning_task.py
+++ improvement-prediction/learning_task.py
@@ -2,8 +2,9 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import KFold 
-from sklearn.metrics import mean_squared_error
-from matplotlib import pyplot as plt
+from util.graphic_functions import *
+from util.metrics import *
+from util.feature_selection import *
 
 SEPARATOR = ','
 
@@ -29,7 +30,7 @@ class LearningTask:
                  #assuming that the relative r-squared gain is in fields[-1]
                  self.learning_data.append(fields[:-1])
                  self.learning_targets.append(fields[-1])
-                 
+
     def execute_linear_regression(self, n_splits):
         kf = KFold(n_splits=n_splits, random_state=42)
         kf.get_n_splits(self.learning_data)
@@ -37,18 +38,24 @@ class LearningTask:
         for train_index, test_index in kf.split(self.learning_data):
             X_train, X_test = np.array(self.learning_data)[train_index], np.array(self.learning_data)[test_index]
             y_train, y_test = np.array(self.learning_targets)[train_index], np.array(self.learning_targets)[test_index]
-            #rf = RandomForestRegressor(n_estimators=100, random_state=42)
-            #rf.fit(X_train, y_train)
-            #predictions = rf.predict(X_test)
             lm = LinearRegression()
             lm.fit(X_train, y_train)
-            predictions = lm.predict(X_test)
-            plt.scatter(y_test, predictions)
-            plt.xlabel('Real values')
-            plt.ylabel('Predicted values')
-            plt.tight_layout()
-            plt.savefig('predicted_r2_score_gains_fold_' + str(i) + '.png', dpi=300)
-            plt.close()
+            mutual_information_univariate_selection(X_train, y_train)
+#             predictions = lm.predict(X_test)
+#             plot_scatterplot(y_test, predictions, 'predicted_r2_score_gains_fold_' + str(i) + '_linear_regression.png', 'Real values', 'Predicted values')
+#             i += 1
+
+    def execute_random_forest(self, n_splits):
+        kf = KFold(n_splits=n_splits, random_state=42)
+        kf.get_n_splits(self.learning_data)
+        i = 0
+        for train_index, test_index in kf.split(self.learning_data):
+            X_train, X_test = np.array(self.learning_data)[train_index], np.array(self.learning_data)[test_index]
+            y_train, y_test = np.array(self.learning_targets)[train_index], np.array(self.learning_targets)[test_index]
+            rf = RandomForestRegressor(n_estimators=100, random_state=42)
+            rf.fit(X_train, y_train)
+            mutual_information_univariate_selection(X_train, y_train)
+            predictions = rf.predict(X_test)
+            plot_scatterplot(y_test, predictions, 'predicted_r2_score_gains_fold_' + str(i) + '_random_forest.png', 'Real values', 'Predicted values')
             i += 1
-            print('Mean squared error:', mean_squared_error(predictions, y_test))
             
