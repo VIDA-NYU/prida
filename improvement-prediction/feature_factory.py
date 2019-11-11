d440c95c371ab10d49e0123b417a4a2bae031e56
Aline Bessa <alibezz@gmail.com>
2019-11-11 17:48:26 -0500

    debugging feature fraction

--- improvement-prediction/feature_factory.py
+++ improvement-prediction/feature_factory.py
@@ -342,8 +342,11 @@ class FeatureFactory:
         return features
 
     def compute_percentual_difference_in_number_of_rows(self, keys_of_another_dataset):
-        """Computes the ratio between the number of rows of the dataset and the number of rows of 
-        another dataset, using keys and their intersections as a proxy
+        """Computes the ratio between the number of rows of the current dataset (potentially the candidate dataset)  
+        and the number of rows of another dataset (potentially the query dataset), using keys and their intersections as a proxy
         """
-        final_number_of_rows = len(set(self.data.index.values) & set(keys_of_another_dataset))  
+
+        #self.data.index.values keeps the keys of self.data, i.e., the current dataset
+        final_number_of_rows = len(set(self.data.index.values) & set(keys_of_another_dataset))
+        #print('final number of rows after integration', final_number_of_rows, 'initial number of rows', len(keys_of_another_dataset))
         return final_number_of_rows/len(keys_of_another_dataset)
