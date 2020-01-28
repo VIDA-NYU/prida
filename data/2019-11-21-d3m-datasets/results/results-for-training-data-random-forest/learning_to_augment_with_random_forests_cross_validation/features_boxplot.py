7d0a09b5874e70c317919fa82b013fc1b98e49a1
Aline Bessa <alibezz@b-192-168-132-6.dynapool.vpn.nyu.edu>
2020-01-28 10:33:46 -0500

    implementing code to tune our classification task

--- data/2019-11-21-d3m-datasets/results/results-for-training-data-random-forest/learning_to_augment_with_random_forests_cross_validation/features_boxplot.py
+++ data/2019-11-21-d3m-datasets/results/results-for-training-data-random-forest/learning_to_augment_with_random_forests_cross_validation/features_boxplot.py
@@ -13,6 +13,8 @@ And then generate a box plot ranking these features.
 import sys
 import matplotlib.pyplot as plt
 
+abbrev = {'query_max_kurtosis': 'q_kurtosis', 'query_max_mean': 'q_mean', 'query_row_column_ratio': 'q_row_to_col', 'query_num_of_columns': 'q_cols', 'query_max_outlier_percentage': 'q_outlier', 'query_max_unique': 'q_unique', 'query_num_of_rows': 'q_rows', 'query_target_max_mutual_info': 'q_mutinfo', 'query_target_max_covariance': 'q_covariance', 'query_target_max_pearson': 'q_pearson', 'query_target_max_spearman': 'q_spearman', 'candidate_max_kurtosis': 'c_kurtosis', 'candidate_max_unique': 'c_unique', 'candidate_num_of_rows': 'c_rows', 'candidate_max_mean': 'c_mean', 'candidate_row_column_ratio': 'c_row_to_col', 'candidate_max_skewness': 'c_skewness', 'candidate_max_outlier_percentage': 'c_outlier', 'candidate_num_of_columns': 'c_cols', 'candidate_max_skewness': 'c_skewness', 'candidate_target_max_mutual_info': 'c_mutinfo', 'candidate_target_max_spearman': 'c_spearman', 'candidate_target_max_pearson': 'c_pearson', 'candidate_target_max_covariance': 'c_covariance', 'containment_fraction': 'containment', 'max_pearson_difference': 'pearson_diff'}
+
 color_dict = {'query_max_kurtosis': 'lightblue', 'query_max_mean': 'lightblue', 'query_row_column_ratio': 'lightblue', 'query_num_of_columns': 'lightblue', 'query_max_outlier_percentage': 'lightblue', 'query_max_unique': 'lightblue', 'query_num_of_rows': 'lightblue', 'query_target_max_mutual_info': 'blue', 'query_target_max_covariance': 'blue', 'query_target_max_pearson': 'blue', 'query_target_max_spearman': 'blue', 'candidate_max_kurtosis': 'pink', 'candidate_max_unique': 'pink', 'candidate_num_of_rows': 'pink', 'candidate_max_mean': 'pink', 'candidate_row_column_ratio': 'pink', 'candidate_max_skewness': 'pink', 'candidate_max_outlier_percentage': 'pink', 'candidate_num_of_columns': 'pink', 'candidate_max_skewness': 'pink', 'candidate_target_max_mutual_info': 'red', 'candidate_target_max_spearman': 'red', 'candidate_target_max_pearson': 'red', 'candidate_target_max_covariance': 'red', 'containment_fraction': 'green', 'max_pearson_difference': 'yellow'}
 
 color_legend = {'lightblue': 'query', 'blue': 'query/target', 'pink': 'candidate', 'red': 'candidate/target', 'green': 'containment', 'yellow': 'pearson diff'}
@@ -23,10 +25,11 @@ def plot_boxplot(features_dict):
     bplot = axes.boxplot([f[1] for f in features],
                              vert=True,  # vertical box alignment
                              patch_artist=True,  # fill with color
-                             labels=[f[0] for f in features])  # will be used to label x-ticks
+                             labels=[abbrev[f[0]] for f in features])  # will be used to label x-ticks
     for label in axes.get_xticklabels():
         label.set_rotation(90)
-    axes.set_title('Feature importances')
+        label.set_fontsize(28)
+    #axes.set_title('Feature importances')
 
     # fill with colors
     colors = [color_dict[f[0]] for f in features]
@@ -40,9 +43,10 @@ def plot_boxplot(features_dict):
         i += 1
     #adding horizontal grid lines
     axes.yaxis.grid(True)
-    axes.set_xlabel('Features')
-    axes.set_ylabel('Mean Decrease Impurity')
-    axes.legend([i for i in indices_for_caption.values()], [i for i in indices_for_caption.keys()], loc='upper right')
+    axes.yaxis.set_tick_params(labelsize=24)
+    #axes.set_xlabel('Features')
+    axes.set_ylabel('Gini Importance', fontsize=28)
+    axes.legend([i for i in indices_for_caption.values()], [i for i in indices_for_caption.keys()], loc='upper right', prop={'size':22})
     plt.savefig('feature-boxplot.png', bbox_inches='tight')
 
 lists = [eval(i.strip()) for i in open(sys.argv[1]).readlines() if '[(' in i]
---
+++ improvement-prediction/classification/class_selection.py
@@ -0,0 +1,90 @@
+""" Given two files (one with training and one with validation data; both with headers), 
+this script selects the threshold alpha above which a gain in R2 squared should correspond to 
+class GOOD GAIN. Anything else will correspond to an instance of class LOSS.
+"""
+
+import pandas as pd
+import sys
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import f1_score, classification_report
+from sklearn.utils import shuffle
+
+def downsample_data(dataset):
+  """This function downsamples the number of instances of a class that is over-represented in the dataset.
+  It's important to keep the learning 'fair'
+  """
+  loss =  dataset.loc[dataset['classes'] == 'loss']
+  good_gain = dataset.loc[dataset['classes'] == 'good_gain']
+  
+  sample_size = min([loss.shape[0], good_gain.shape[0]])
+  loss = loss.sample(n=sample_size, random_state=42)
+  good_gain = good_gain.sample(n=sample_size, random_state=42)
+  
+  frames = [loss, good_gain]
+  return shuffle(pd.concat(frames), random_state=0)
+
+
+def determine_classes_based_on_gain_in_r2_score(dataset, alpha, downsample=True):
+  """This function determines the class of each row in the dataset based on the value 
+  of column 'gain_in_r2_score'
+  """
+  gains = dataset['gain_in_r2_score']
+  classes = ['good_gain' if i > alpha else 'loss' for i in gains]
+  dataset['classes'] = classes
+  if downsample:
+    return downsample_data(dataset)
+  return dataset
+
+ALPHA_GRID = [0.1 * x for x in range(10)]
+if __name__ == '__main__':
+
+  training_filename = sys.argv[1]
+  validation_filename = sys.argv[2]
+
+  training = pd.read_csv(training_filename)
+  validation = pd.read_csv(validation_filename)
+  
+  max_f1score = float('-inf')
+  best_alpha = -1
+  for alpha in ALPHA_GRID:
+    tmp_training = determine_classes_based_on_gain_in_r2_score(training, alpha, downsample=False)
+    tmp_validation = determine_classes_based_on_gain_in_r2_score(validation, alpha, downsample=False)
+
+    X_train = tmp_training[['query_num_of_columns', 'query_num_of_rows', 'query_row_column_ratio',
+       'query_max_mean', 'query_max_outlier_percentage', 'query_max_skewness',
+       'query_max_kurtosis', 'query_max_unique', 'candidate_num_of_columns',
+       'candidate_num_rows', 'candidate_row_column_ratio',
+       'candidate_max_mean', 'candidate_max_outlier_percentage',
+       'candidate_max_skewness', 'candidate_max_kurtosis',
+       'candidate_max_unique', 'query_target_max_pearson',
+       'query_target_max_spearman', 'query_target_max_covariance',
+       'query_target_max_mutual_info', 'candidate_target_max_pearson',
+       'candidate_target_max_spearman', 'candidate_target_max_covariance',
+       'candidate_target_max_mutual_info', 'max_pearson_difference',
+       'containment_fraction']]
+    y_train = tmp_training['classes']
+
+    X_test = tmp_validation[['query_num_of_columns', 'query_num_of_rows', 'query_row_column_ratio',
+       'query_max_mean', 'query_max_outlier_percentage', 'query_max_skewness',
+       'query_max_kurtosis', 'query_max_unique', 'candidate_num_of_columns',
+       'candidate_num_rows', 'candidate_row_column_ratio',
+       'candidate_max_mean', 'candidate_max_outlier_percentage',
+       'candidate_max_skewness', 'candidate_max_kurtosis',
+       'candidate_max_unique', 'query_target_max_pearson',
+       'query_target_max_spearman', 'query_target_max_covariance',
+       'query_target_max_mutual_info', 'candidate_target_max_pearson',
+       'candidate_target_max_spearman', 'candidate_target_max_covariance',
+       'candidate_target_max_mutual_info', 'max_pearson_difference',
+       'containment_fraction']]
+    y_test = tmp_validation['classes']
+
+    clf = RandomForestClassifier(random_state=42)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    f1score = f1_score(y_test, y_pred, pos_label='good_gain')
+    if f1score > max_f1score:
+      max_f1score = f1score
+      best_alpha = alpha
+    print('alpha', alpha, 'f1score', f1score)
+    print(classification_report(y_test, y_pred))
+  print('best alpha', best_alpha, 'f1-score', max_f1score)
