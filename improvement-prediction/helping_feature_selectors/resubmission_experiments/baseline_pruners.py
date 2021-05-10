e315c704c5136480ebf78147efe1aac4438934f3
Aline Bessa <alibezz@10-18-178-246.dynapool.wireless.nyu.edu>
2021-05-10 17:58:52 -0400

    making progress in the comparison between hierarchical prida and other methods

--- improvement-prediction/helping_feature_selectors/resubmission_experiments/baseline_pruners.py
+++ improvement-prediction/helping_feature_selectors/resubmission_experiments/baseline_pruners.py
@@ -1,5 +1,5 @@
 '''
-Here, we implement baseline pruners such as the classic version of PRIDA and 
-a containment-based method.
+Here, we implement baseline pruners such as the classic version of PRIDA, a 
+regression-based version, and a containment-based method.
 '''
 
